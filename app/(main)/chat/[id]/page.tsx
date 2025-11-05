94932bebd9ca2a6602ae163505b1cf7bdf6debb3
resilience <547471919@qq.com>
2025-11-05 22:51:44 +0800

    feat: 多轮对话页面 布局设计

---
+++ app/(main)/chat/[id]/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { ChatInputBox } from '../_components/chat-input-box'
+import { ChatWelcomeHeader } from '../_components/chat-welcome-header'
+
+export default function ConversationPage() {
+  return (
+    <div className="flex flex-col h-full ">
+      <ChatWelcomeHeader />
+
+      {/* 消息列表区域 */}
+      <div className="flex-1 overflow-y-auto">
+      </div>
+
+      {/* 输入框区域 */}
+      <ChatInputBox />
+    </div>
+  )
+}
