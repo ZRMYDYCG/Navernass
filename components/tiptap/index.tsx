c74d0703810bec74cf5f33e238fc6a1fb998c170
rimin.zeng <rimin.zeng@yidutech.com>
2025-11-17 15:23:33 +0800

    feat(editor): 官方 DragHandle 扩展替换自定义拖拽

--- components/tiptap/index.tsx
+++ components/tiptap/index.tsx
@@ -1,6 +1,4 @@
-export { DragHandle } from './drag-handle'
+export { DragHandle } from './drag-handle-react'
 export { FloatingMenu } from './floating-menu'
 export { TiptapEditor, useTiptapEditor } from './tiptap-editor'
 export type { TiptapEditorProps, TiptapEditorStats } from './tiptap-editor'
-
-
